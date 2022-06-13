c4bfc3822281546f44ef1ac624494c9bf49ec564
Ivan-Krul <kiwiivanya128@gmail.com>
2022-06-13 23:37:04 +0300

    Добавьте файлы проекта.

---
+++ BooleanCompiler/BooleanCompiler.cpp
@@ -0,0 +1,11 @@
+﻿// BooleanCompiler.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+int main(size_t args, char* argv[]) {
+	for(size_t i = 0; i < args; i++)
+		std::cout << argv[i]<<'\n';
+	std::cout << "Hello CMake.\n";
+	return 0;
+}
